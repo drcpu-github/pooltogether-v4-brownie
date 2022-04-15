46452a8d3cb72b5bce9b839169f0cba44181d0b1
drcpu <drcpu@protonmail.com>
2022-04-15 14:14:19 +0200

    [scripts] Fixed pick sorting bug in claim_prizes script and added a test transaction

--- scripts/claim_prizes.py
+++ scripts/claim_prizes.py
@@ -1,7 +1,10 @@
 import json
+import sys
 import time
 
-from brownie import accounts, config, Contract
+from brownie import accounts, config, Contract, web3
+
+from web3 import Web3
 
 from classes.draw_calculator import DrawCalculator
 from classes.helper import Helper
@@ -40,11 +43,15 @@ def claim_prizes(options, network):
     abi_prize_distributor = json.loads(open("abis/PrizeDistributorAbi.json").read())
     prize_distributor_contract = Contract.from_abi("PrizeDistributor", options["contracts"][network]["prize_distributor"], abi_prize_distributor)
 
+    mock_prize_distributor_contract = web3.eth.contract(address = options["contracts"][network]["prize_distributor"], abi = abi_prize_distributor)
+
     draw_calculator = DrawCalculator(network)
 
     # Setup account to use for claiming prizes
     accounts.add(config["wallets"]["from_key"])
     claiming_account = accounts[0]
+    claiming_account_str = f"{claiming_account}"
+    print(f"Claiming prizes using account {claiming_account_str}")
 
     helper = Helper()
 
@@ -54,7 +61,6 @@ def claim_prizes(options, network):
         # Fetch prizes from database for account and network
         draws = helper.get_prizes(options["config"], network, account)
 
-        total_value_claimable = 0
         unclaimed_draws, unclaimed_draw_ids = [], []
         for draw in draws:
             # Cannot claim prizes for the newest draw due to the timelock
@@ -64,17 +70,27 @@ def claim_prizes(options, network):
             if draw["total_value_claimable"] > 0:
                 payed_out = prize_distributor_contract.getDrawPayoutBalanceOf(account, draw["draw_id"])
                 if payed_out == 0:
-                    total_value_claimable += draw["total_value_claimable"]
+                    # Apparently the DrawCalculator contract expects picks to be sorted from small to large
+                    draw["claimable_prizes"] = sorted(draw["claimable_prizes"], key=lambda l: l["pick"])
                     unclaimed_draws.append(draw)
                     unclaimed_draw_ids.append(draw["draw_id"])
                 else:
-                    print(f"Already claimed prizes for account {account}, draw id {draw['draw_id']}")
+                    print(f"Already claimed prizes for draw id {draw['draw_id']} for account {account}")
+
+        print(f"Creating claim for draws {unclaimed_draw_ids}")
 
         # Create claims
         claim = draw_calculator.prepare_claims(
             account,
             unclaimed_draws,
         )
+
+        # Test claim transaction
+        try:
+            estimated_gas = mock_prize_distributor_contract.functions.claim(Web3.toChecksumAddress(account), unclaimed_draw_ids, claim["encoded_winning_pick_indices"]).estimateGas({"from": claiming_account_str})
+        except ValueError as e:
+            print(f"Will fail to claim for draws {unclaimed_draw_ids}: {e}")
+            sys.exit(1)
 
         # Claim
         prize_distributor_contract.claim(
