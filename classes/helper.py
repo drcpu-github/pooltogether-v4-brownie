b3875668032e277723f921c2ac408b04c3cb621e
underethsea <90292032+underethsea@users.noreply.github.com>
2022-07-08 14:38:41 -0700

    add optimism
    requires OPTIMISM_API_KEY

--- classes/helper.py
+++ classes/helper.py
@@ -248,6 +248,9 @@ class Helper:
         elif network == "ethereum":
             base_url = "https://api.etherscan.io/api"
             api_key = os.getenv("ETHERSCAN_API_KEY")
+        elif network == "optimism":
+            base_url = "https://api-optimistic.etherscan.io/api"
+            api_key = os.getenv("OPTIMISM_API_KEY")
         else:
             raise ValueError("Unknown network")
         return base_url, api_key
